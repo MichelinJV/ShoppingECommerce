6b2b97ab447c5f98acb2e2b443f0fabbe62977af
MichelinJV <michelinjv@gmail.com>
2022-05-07 11:12:45 -0300

    Comitando

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "camiseta",
+      fk_idcategorys: 1,
+      name_product: "Minato",
+      price: 300,
+      image: "./images/produtos/minatocamisa.jpg",
+      id_categorys: 1,
+      name_categorys: "Camiseta Masculina",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "camiseta",
+      fk_idcategorys: 1,
+      name_product: "Haikyuu",
+      price: 350,
+      image: "./images/produtos/haikyuucamisa.jpg",
+      id_categorys: 1,
+      name_categorys: "Camiseta Masculina",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "camiseta",
+      fk_idcategorys: 1,
+      name_product: "Grêmio 2020",
+      price: 230,
+      image: "./images/produtos/dragonballcamisa.jpg",
+      id_categorys: 1,
+      name_categorys: "Camiseta Masculina",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "blusinha",
+      fk_idcategorys: 2,
+      name_product: "Roy Mustang",
+      price: 399.99,
+      image: "./images/produtos/royblusinha.jpg",
+      id_categorys: 2,
+      name_categorys: "Camiseta Feminina",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "blusinha",
+      fk_idcategorys: 2,
+      name_product: "Totoro",
+      price: 350.99,
+      image: "./images/produtos/totoroblusinha.jpg",
+      id_categorys: 2,
+      name_categorys: "Camiseta Feminina",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "blusinha",
+      fk_idcategorys: 2,
+      name_product: "Full Metal",
+      price: 299.99,
+      image: "./images/produtos/fullmetalblusinha.jpg",
+      id_categorys: 2,
+      name_categorys: "Camiseta Feminina",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "moleton",
+      fk_idcategorys: 3,
+      name_product: "Attack on Titan",
+      price: 200,
+      image: "./images/produtos/attackmoleton.jpg",
+      id_categorys: 3,
+      name_categorys: "Moleton Masculino",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "moleton",
+      fk_idcategorys: 3,
+      name_product: "Kimetsu",
+      price: 450,
+      image: "./images/produtos/kimetsumoletom.jpg",
+      id_categorys: 3,
+      name_categorys: "Moleton Masculino",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "agasalho",
+      fk_idcategorys: 4,
+      name_product: "Sailor Moon",
+      price: 350,
+      image: "./images/produtos/sailormoleton.jpg",
+      id_categorys: 4,
+      name_categorys: "Moleton Feminino",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "agasalho",
+      fk_idcategorys: 4,
+      name_product: "Akatsuki",
+      price: 399,
+      image: "./images/produtos/akatsukimoleton.jpg",
+      id_categorys: 4,
+      name_categorys: "Moleton Feminino",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "hentai",
+      fk_idcategorys: 5,
+      name_product: "Hentai",
+      price: 180,
+      image: "./images/produtos/hentaicamisa.jpeg",
+      id_categorys: 5,
+      name_categorys: "Hentai",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "hentai",
+      fk_idcategorys: 5,
+      name_product: "Hentai",
+      price: 189.99,
+      image: "./images/produtos/hentai2camisa.jpg",
+      id_categorys: 5,
+      name_categorys: "Hentai",
+      selected: false
+    },
+];
+
+export default productsList;
